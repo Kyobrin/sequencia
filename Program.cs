8b7af19330467f7e4634355c7c265e81a6c484b3
kyobrin <zullisun@gmail.com>
2022-11-18 20:43:14 -0300

    sequencia

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿{
+Console.WriteLine("\n-------------------------");
+Console.WriteLine("\n    SEQUENCIA DE PARES   ");
+Console.WriteLine("\n-------------------------");
+int comeco = 0;
+System.Console.WriteLine("Numeros pares entre 0 e");
+int numero_escrito = Convert.ToInt32(Console.ReadLine());
+while ( comeco <= numero_escrito)
+{
+    Console.Write($"{comeco} ");
+    comeco += 2;
+}
+
+}
